275a68352ab5bb71e81ef04d5105c886cec16aad
Nixus <54758964+nickcav-aus@users.noreply.github.com>
2023-01-30 08:42:27 +1100

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+touch API.KEY
+# add your API Key to the file above
+apikey=$(cat API.KEY)
+url=$1
+
+curl -X POST "https://urlscan.io/api/v1/scan/" \
+      -H "Content-Type: application/json" \
+      -H "API-Key: $apikey" \
+      -d "{ \
+        \"url\": \"$url\", \"public\": \"on\", \
+        \"tags\": [\"phishing\", \"malicious\"] \
+      }"
